8da0006355be7aabeee882f6c69666b3e4ac1cf3
TotoPhandolack <totophandolack915@gmail.com>
2026-09-25 15:14:39 +0700

    + owner path

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 import Navbar from "./components/Navbar";
+import { usePathname } from "next/navigation";
 const page = () => {
+
+  const isOwnerPath = usePathname().includes("owner");
+
   return (
     <div>
-      <Navbar />
+      {!isOwnerPath && <Navbar />}
     </div>
   )
 }
